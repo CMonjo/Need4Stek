a188f1970fa2007b4478fc9f7190a1aaa7729884
alexandre.finne <alexandre.finne@epitech.eu>
2018-05-20 20:44:42 +0200

    Array [Feat] : Modifing the values of the arrays.

--- src/n4s.c
+++ src/n4s.c
@@ -10,20 +10,22 @@
 speed_t speed[] = {
 	{2000, "1\n", 2},
 	{1500, "0.8\n", 4},
+	{1000, "0.65\n", 5},
 	{1000, "0.5\n", 4},
 	{600, "0.4\n", 4},
 	{400, "0.2\n", 4},
-	{200, "0.1\n", 4},
-	{0, "0.3\n", 4},
+	{100, "0.1\n", 4},
+	{75, "0.3\n", 2},
 };
 
 direction_t direction[] = {
-	{1500, "0.005\n", 6},
-	{1000, "0.05\n", 5},
-	{600, "0.1\n", 4},
-	{400, "0.2\n", 4},
-	{200, "0.3\n", 4},
-	{0, "0.5\n", 4},
+	{1500, "0.01\n", 5},
+	{1000, "0.1\n", 4},
+	{600, "0.15\n", 5},
+	{400, "0.25\n", 5},
+	{300, "0.40\n", 5},
+	{200, "0.5\n", 4},
+	{100, "0.75\n", 5},
 };
 
 nfs_t *init_nfs(void)
@@ -63,7 +65,7 @@ void get_lidar(nfs_t *nfs)
 	rays = my_str_to_word_array(buf, ':');
 	for (int i = 0; i != 32; i++)
 		nfs->rays[i] = atof(rays[i + 3]);
-	nfs->mid = nfs->rays[14];
+	nfs->mid = nfs->rays[15];
 	game_end(buf);
 }
 
@@ -72,11 +74,11 @@ void set_speed(nfs_t *nfs)
 	char *buf;
 	size_t buf_size = 0;
 
-	if (nfs->mid <= 199)
+	if (nfs->mid <= 75)
 		write(1, "CAR_BACKWARDS:", 14);
 	else
 		write(1, "CAR_FORWARD:", 12);
-	for (int i = 0; i != 7; i++) {
+	for (int i = 0; i != 8; i++) {
 		if (nfs->mid >= speed[i].mid_s) {
 			write(1, speed[i].set_speed, speed[i].len);
 			break;
@@ -95,7 +97,7 @@ void set_alpha(nfs_t *nfs)
 	write(1, "WHEELS_DIR:", 11);
 	if (dir < 0)
 		write(1, "-", 1);
-	for (int i = 0; i != 6; i++) {
+	for (int i = 0; i != 7; i++) {
 		if (nfs->mid >= direction[i].mid_s) {
 			write(1, direction[i].set_direction, direction[i].len);
 			break;
